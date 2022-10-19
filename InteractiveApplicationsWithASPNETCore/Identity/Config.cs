732b8b4dc6de10f8d886ca94fb0c2264651f2c14
Graham Bull <graham.bull@gmail.com>
2022-10-19 22:56:01 +0100

    ready to create oidc client
    https://docs.duendesoftware.com/identityserver/v6/quickstarts/2_interactive/#create-the-oidc-client

--- InteractiveApplicationsWithASPNETCore/Identity/Config.cs
+++ InteractiveApplicationsWithASPNETCore/Identity/Config.cs
@@ -1,13 +1,15 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Identity;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
+        new List<IdentityResource>
         {
-            new IdentityResources.OpenId()
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -34,6 +36,26 @@ public static class Config
 
                 // scopes that client has access to
                 AllowedScopes = { "api" }
+            },
+            // interactive ASP.NET Core Web App
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+            
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                }
             }
         };
 }
